b2ec55e5bf504a05bdb1272caad9f27bee1d021d
BroTato123 <42946604+BroTato123@users.noreply.github.com>
2019-10-08 18:59:51 -0400

    Update bot.js
    Made one for "minecraft" and "mother russia"

--- bot.js
+++ bot.js
@@ -205,7 +205,9 @@ client.on('message', async message => {
             else if (msg.includes("factorio") && msg.includes("satis")) message.channel.send('The unholy combination');
             else if (msg.includes("satisfactory")) message.channel.send("Factorio 2: Electric Boogaloo");
             else if (msg.includes("factorio")) message.channel.send("Good game");
-            else if (msg.includes("apex")) message.channel.send('Is Apex the *peak* of gaming?');
+            else if (msg.includes("apex")) message.channel.send('Apex is not the *peak* of gaming');
+            else if (msg.includes("minecraft")) message.channel.send('Minecraft is the *peak* of gaming');
+            else if (msg.includes("mother russia")) message.channel.send('Союз нерушимый республик свободных Сплотила навеки Великая Русь. Да здравствует созданный волей народов Единый, могучий Советский Союз! Славься, Отечество наше свободное, Дружбы, народов надежный оплот! Знамя советское, знамя народное Пусть от победы, к победе ведет! Сквозь грозы сияло нам солнце свободы, И Ленин великий нам путь озарил. Нас вырастил Сталин - на верность народу На труд и на подвиги нас вдохновил. Славься, Отечество чаше свободное, Счастья народов надежный оплот! Знамя советское, знамя народное Пусть от победы к победе ведет! Skvoz grozy siialo nam solntse svobody, I Lenin velikij nam put ozaril. Nas vyrastil Stalin - na vernost narodu Na trud i na podvigi nas vdokhnovil. Slavsia, Otechestvo chashe svobodnoe, Schastia narodov nadezhnyj oplot! Znamia sovetskoe, znamia narodnoe Pust ot pobedy k pobede vedet! Мы армию нашу растили в сраженьях, Захватчиков подлых с дороги сметем! Мы в битвах решаем судьбу поколений, Мы к славе Отчизну свою поведем! Славься, Отечество наше свободное, Славы народов надежный оплот! Знамя советское, знамя народное Пусть от победы к победе ведет!');
         }
     }
 });
